2ca8ad7f01636105beef269c6d7b17f505ded815
SMA Bari <mdazizulbarishafin@gmail.com>
2024-07-18 21:38:05 +0600

    another way of break

--- javascript/index.js
+++ javascript/index.js
@@ -100,9 +100,21 @@ function breakNumber(number){
     for(let i=0; i<20; i++){
         if(i === number){
             break
-        } if(i % 2 === 1){
-            continue
-        } evenNumbers4.push(i)
+        } if(i % 2 === 0){
+            evenNumbers4.push(i)
+        } 
     } return evenNumbers4
+}
+console.log(breakNumber(10))
+
+function breakNumber(number){
+    let evenNumbers4 = []
+    for(let i=0; i<20; i++){
+        if(i === number){
+            return evenNumbers4
+        } if(i % 2 === 0){
+            evenNumbers4.push(i)
+        } 
+    } 
 }
 console.log(breakNumber(10))
