dacc8c3e387019913c3dac4d684ef793229ccfe0
SMA Bari <mdazizulbarishafin@gmail.com>
2024-07-18 14:10:21 +0600

    js values

---
+++ javascript/index.js
@@ -0,0 +1,8 @@
+const friends = [22, true, "Bari"]
+const bigIntNumber = 11111111111111111111111111n
+let someValue = null
+// saying that this value dosen't exist
+let undefinedValue
+// value can be defined later
+console.log(friends)
+
