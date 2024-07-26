f32d95b2ec0b18d19f45c6082765e38612a5bebe
SMA Bari <mdazizulbarishafin@gmail.com>
2024-07-26 13:36:48 +0600

    some practicing

--- chromedev-tools/index.js
+++ chromedev-tools/index.js
@@ -1,3 +1,17 @@
 function logSomething() {
     console.log(Math.random())
 }
+console.warn("This is a warning message.")
+console.error("This is an error message.")
+console.table([
+    {
+        firstName : "Azizul",
+        lastName : "Bari"
+    },
+    {
+        firstName : "Imrul",
+        lastName : "Bari",
+        birthDay : "7th Jan"
+    }
+])
+console.log("%cHello CSS!", "font-size: 40px; font-family: Arial")
