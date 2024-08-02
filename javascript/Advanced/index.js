8e4ed5c3624e6c480db4cc865e101cfd960113df
sma-bari <mdazizulbarishafin@gmail.com>
2024-08-02 06:46:19 +0600

    try catch... trying to catch an error

--- javascript/Advanced/index.js
+++ javascript/Advanced/index.js
@@ -10,3 +10,21 @@ let intervalId = setInterval(() => {
         clearInterval(intervalId)
     }
 }, 1000);
+
+
+// try catch... trying to catch an error
+let i2 = 100;
+let j2 = 0;
+try{
+    if(j2 == 0){
+        throw `I can't be zero`
+    }
+} catch(err){
+    console.log(err)
+} // using this without try catch will breake the code
+
+try{
+    document.getElementById(`abc`).innerText = `100`
+} catch(err){
+    console.log(err)
+}
