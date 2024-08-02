91bb670157c326f90275ce0c7506f73692926151
sma-bari <mdazizulbarishafin@gmail.com>
2024-08-02 13:25:07 +0600

    adding semicolons to see if the bug goes away

--- javascript/Advanced/2d-cargame/index.js
+++ javascript/Advanced/2d-cargame/index.js
@@ -1,52 +1,53 @@
-const score = document.querySelector(`.score`)
-const startScreen = document.querySelector(`.startScreen`)
-const gameArea = document.querySelector(`.gameArea`)
+const score = document.querySelector(`.score`);
+const startScreen = document.querySelector(`.startScreen`);
+const gameArea = document.querySelector(`.gameArea`);
 
 let keys = {
     ArrowUp: false,
     ArrowDown: false,
     ArrowRight: false,
     ArrowLeft: false,
-}
-let player = {speed:5}
+};
+let player = {speed:5};
 
-startScreen.addEventListener(`click`, start)
-document.addEventListener(`keydown`, pressOn)
-document.addEventListener(`keyup`, pressOff)
+startScreen.addEventListener(`click`, start);
+document.addEventListener(`keydown`, pressOn);
+document.addEventListener(`keyup`, pressOff);
 
 function playGame(){
-    console.log(`inplay`)
-    let car = document.querySelector(`.car`)
+    console.log(`inplay`);
+    let car = document.querySelector(`.car`);
     if(player.start){
-        if(keys.ArrowUp){player.y -= player.speed}
-        if(keys.ArrowDown){player.y += player.speed}
-        if(keys.ArrowLeft){player.x -= player.speed}
-        if(keys.ArrowRight){player.x += player.speed}
-        car.style.left = player.x + `px`
-        car.style.top = player.y + `px`
-        window.requestAnimationFrame(playGame)
-    }
-}
+        if(keys.ArrowUp){player.y -= player.speed;};
+        if(keys.ArrowDown){player.y += player.speed;};
+        if(keys.ArrowLeft){player.x -= player.speed;};
+        if(keys.ArrowRight){player.x += player.speed;};
+        car.style.left = player.x + `px`;
+        car.style.top = player.y + `px`;
+        window.requestAnimationFrame(playGame);
+    };
+};
 function pressOn(e){
     e.preventDefault()
-    keys[e.key] = true
-    console.log(keys)
-}
+    keys[e.key] = true;
+    console.log(keys);
+};
 function pressOff(e){
     e.preventDefault()
-    keys[e.key] = false
-    console.log(keys)
-}
+    keys[e.key] = false;
+    console.log(keys);
+};
 function start(){
-    startScreen.classList.add(`hide`)
-    gameArea.classList.remove(`hide`)
-    player.start = true
-    window.requestAnimationFrame(playGame)
-    let car = document.createElement(`div`)
-    car.innerHTML = `<p class='car'>Car</p>`
+    startScreen.classList.add(`hide`);
+    gameArea.classList.remove(`hide`);
+    player.start = true;
+    window.requestAnimationFrame(playGame);
+    let car = document.createElement(`div`);
+    car.innerText = `Car`;
+    car.setAttribute(`class`, `car`);
 
-    gameArea.appendChild(car)
-    player.x = car.offsetLeft
-    player.y = car.offsetTop
-    console.log(player)
-}
+    gameArea.appendChild(car);
+    player.x = car.offsetLeft;
+    player.y = car.offsetTop;
+    console.log(player);
+};
