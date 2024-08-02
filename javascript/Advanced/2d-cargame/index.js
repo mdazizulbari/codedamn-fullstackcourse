f266e22019eebb2ab204e6b13bf87e556605ee67
sma-bari <mdazizulbarishafin@gmail.com>
2024-08-02 19:40:20 +0600

    I quite trying to fight with old code

--- javascript/Advanced/2d-cargame/index.js
+++ javascript/Advanced/2d-cargame/index.js
@@ -25,9 +25,22 @@ function moveLines(){
         item.style.top = item.y + `px`;
     })
 }
+function moveEnemy(){
+    let ele = document.querySelectorAll(`.enemy`)
+    ele.forEach(function(item){
+        console.log(item.y);
+        if(item.y >= 1500){
+            item.y -= 1500;
+            item.style.left = Math.floor(Math.random()*150) + `px`;
+        }
+        item.y += player.speed;
+        item.style.top = item.y + `px`;
+    })
+}
 function playGame(){
     let car = document.querySelector(`.car`);
     moveLines();
+    moveEnemy();
     let road = gameArea.getBoundingClientRect();
     if(player.start){
         if(keys.ArrowUp && player.y > road.top){
@@ -63,8 +76,8 @@ function start(){
     player.start = true;
     for(let x=0;x<10;x++){
         let div = document.createElement(`div`);
-        div.classList.add(`line`)
-        div.y = x * 150
+        div.classList.add(`line`);
+        div.y = x * 150;
         div.style.top = (x*150)+ `px`;
         gameArea.appendChild(div);
     }
@@ -76,5 +89,13 @@ function start(){
     gameArea.appendChild(car);
     player.x = car.offsetLeft;
     player.y = car.offsetTop;
-    console.log(player);
+    for(let x=0;x<3;x++){
+        let enemy = document.createElement(`div`);
+        enemy.classList.add(`enemy`);
+        enemy.y = ((x+1)*600) *- 1;
+        enemy.style.top = enemy.y + `px`;
+        enemy.style.left = Math.floor(Math.random()*150) + `px`;
+        enemy.style.backgroundColor = `red`;
+        gameArea.appendChild(enemy);
+    }console.log(player);
 };
